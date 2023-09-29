ed21d0d772c58c1e1f3a04a61f4df6378168acbc
ICX\Yan.Herasimovich <yherasimovich@suvoda.com>
2023-09-29 18:27:30 +0300

    add drafts

---
+++ src/main/java/service/informedConsent/model/DraftQuery.java
@@ -0,0 +1,38 @@
+package service.informedConsent.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DraftQuery {
+    @JsonProperty("Name")
+    private String name;
+
+    @JsonProperty("Id")
+    private String id;
+
+    @JsonProperty("Code")
+    private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
